c739ae41d996efdbb675ee8081f20aa25dd23091
Augusto Konig <augustokonig@gmail.com>
2023-02-06 19:21:19 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mobileapps1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val helloText = findViewById<TextView>(R.id.welcomeText)
+        helloText.text = getString(R.string.hello_dorset)
+
+        val welcomeText = findViewById<TextView>(R.id.welcomeNameText)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        val nameEditText = findViewById<EditText>(R.id.nameEditText)
+        val nameText = nameEditText.text
+        saveButton.setOnClickListener {
+            val welcomeTextString = "Welcome, $nameText!"
+            welcomeText.text = welcomeTextString
+        }
+
+        val nextPageButton = findViewById<Button>(R.id.nextPageButton)
+        nextPageButton.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
+
+
